a905b3c70be565945fd563820ce1d96614a22aac
Paul Durivage <1240740+angstwad@users.noreply.github.com>
2023-11-22 16:25:12 -0700

    actual initial commit

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # perpetual-touch-id-for-sudo
-Enables Touch ID for sudo automagically after OS updates screw it up!
+
+## Wtf
+
+Touch ID can be used for passwordless sudo in your terminal.  You just need to add the following line into your `/etc/pam.d/sudo` file:
+
+```
+auth sufficient pam_tid.so
+```
+
+But every time you upgrade, macOS blows this change away.
+
+This script helps you keep your `/etc/pam.d/sudo` up-to-date so you don't need to fix this file with every system upgrade.
+
+## How to Use
+
+Drop `pam_tid.py` somewhere on your file system.  I keep mine at `~/.local/bin/pam_tid.py`.  I made mine executable with the following:
+
+```bash
+chmod u+x ~/.local/bin/pam_tid.py
+```
+
+Add the following function into your shell `rc` file.  Because I use bash, I appended this to my `.bashrc`:
+
+```bash
+function sudo {
+    ${HOME}/.local/bin/pam_tid.py --check || echo "Enabling Touch ID for sudo..."; /usr/bin/sudo ${HOME}/.local/bin/pam_tid.py
+    /usr/bin/sudo "$@"
+}
+```
+
+That's it!  Now if you try to run `sudo` and the Touch ID isn't enabled, it'll fix it before proceeding.  
---
+++ pam_tid.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+"""
+Copyright (c) 2023 Paul Durivage
+"""
+import argparse
+import pathlib
+import re
+import sys
+
+LINE = "\nauth sufficient pam_tid.so"
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Ensures Touch ID is ")
+    parser.add_argument("--check", action="store_true")
+    parser.add_argument("--file", type=str, default="/etc/pam.d/sudo")
+    args = parser.parse_args()
+
+    path = pathlib.Path(args.file)
+
+    try:
+        assert path.exists(), f"{path}: does not exist"
+        assert path.is_file(), f"{path}: not a file"
+    except AssertionError as e:
+        raise SystemExit(str(e))
+
+    with open(path) as f:
+        data = f.read()
+
+    p_tid = re.compile(r'^auth\s+sufficient\s+pam_tid\.so$',
+                       flags=re.MULTILINE)
+
+    match_tid = re.search(p_tid, data)
+
+    if args.check and match_tid:
+        sys.exit(0)
+    elif args.check and not match_tid:
+        sys.exit(1)
+
+    if not match_tid:
+        p_auth_suff = re.compile(r"^auth\s+sufficient\s+pam_\w+\.so$",
+                                 flags=re.MULTILINE)
+        match = list(re.finditer(p_auth_suff, data))[-1]
+        s_out = data[:match.end()] + LINE + data[match.end():]
+
+        with open(path, 'w') as f:
+            f.write(s_out)
+
+
+if __name__ == '__main__':
+    main()
